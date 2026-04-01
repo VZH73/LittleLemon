2e90cf78ead39775be15eed757859e305f949f32
VZH73 <30855956+VZH73@users.noreply.github.com>
2026-04-01 22:40:42 +0300

    views added

---
+++ restaurant/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Booking, Menu
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class MenuSerializer(ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
